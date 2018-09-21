7525ee41720c9923fdae4eb364d66a2bcc95c4dd
Maximiliano Mercado <maximilianomercado.n@gmail.com>
2018-09-21 15:26:07 -0300

    Estructura de carpetas

--- Ejercicio 5/TP2-Ejercicio5.sh
+++ Ejercicio 5/TP2-Ejercicio5.sh
@@ -9,8 +9,12 @@ verificar_path()
     touch $1 ; echo 'IdPersona;DNI;Apellido_y_Nombre;idPais' > $1;    
   fi
 
-
-
+  if [ -f $2 ]
+  then 
+    echo 'Ok'
+  else
+    touch $2 ; echo 'IdPais;Nombre' > $2;
+  fi
  exit 0
 }
 
