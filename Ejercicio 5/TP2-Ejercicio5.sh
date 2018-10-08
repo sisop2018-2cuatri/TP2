07834553b08691928e45d1100c5a8edfa6ef01f9
Maximiliano Mercado <maximilianomercado.n@gmail.com>
2018-10-08 06:45:58 -0300

    Agrego mas modificaciones

--- Ejercicio 5/TP2-Ejercicio5.sh
+++ Ejercicio 5/TP2-Ejercicio5.sh
@@ -4,15 +4,14 @@ verificar_path()
 {
   if [ -f $1 ]
   then
-    echo 'Ok'
+    echo ''
   else
-  echo "que paso"
     touch personas.txt ; echo 'IdPersona;DNI;Apellido_y_Nombre;idPais' > personas.txt;    
   fi
 
   if [ -f $2 ]
   then 
-    echo 'Ok'
+    echo ''
   else
     touch paises.txt ; echo 'IdPais;Nombre' > paises.txt;
   fi
@@ -28,11 +27,28 @@ case $1 in
 	 -d|-D)
 	    
 		DNI=$2
-		awk  -F ";" '$2==var {print $0}' var="$DNI" $dirPersonas;;
+		#awk  -F ";" '$2 == var {print $0}' var="$DNI" $dirPersonas;;
+		awk -v var=$DNI -F ";"  '{
+						
+					if ( $2 == var )
+					{
+						print $0;	
+				    }
+					else	
+					{
+						print "Error DNI inexistente";
+					}
+				}' $dirPersonas;;
 		
 	 -a|-A)
          
-         awk -F ";" '$1=="" {printf $1";"$2";"$3";"$4}' $1>$1;;
+		 numLineas=$(wc -l < "$dirPersonas")
+		 
+		 
+		 awk -v id=$(expr $numLineas - 1) dni=$2 apyn=$3 pais=$4 -F ";"  '{print id";"dni";"apyn";"pais}'  
+		 
+		 ;;
+         #awk -F ";" '$1=="" {printf $1";"$2";"$3";"$4}' $1>$1;;
          
      -e|-E)
          
@@ -42,7 +58,7 @@ case $1 in
      -p|-P)
          
          #awk -F ";" '$2 != 35537983 {print}' $1;;
-         awk - F ";" ' $4==1 {a[$1]=$4; next} $2 in a {print $1,$2,$3,a[$2]}' OFS=';' $2 $1;;
+         awk - F ";" ' $4==1 {a[$1]=$4; next} $2 in a {print $1,$2,$3,a[$ -2]}' OFS=';' $2 $1;;
          
 	*)    exit;;
 esac
