e5478e4169dbae65147b526061f88d6a95a3a8b7
Maximiliano Mercado <maximilianomercado.n@gmail.com>
2018-10-08 01:26:57 -0300

    Correcciones al ejercicio 5, ya se puede listar la persona, hay que retocarlo un poco mas

--- Ejercicio 5/TP2-Ejercicio5.sh
+++ Ejercicio 5/TP2-Ejercicio5.sh
@@ -6,16 +6,43 @@ verificar_path()
   then
     echo 'Ok'
   else
-    touch $1 ; echo 'IdPersona;DNI;Apellido_y_Nombre;idPais' > $1;    
+  echo "que paso"
+    touch personas.txt ; echo 'IdPersona;DNI;Apellido_y_Nombre;idPais' > personas.txt;    
   fi
 
   if [ -f $2 ]
   then 
     echo 'Ok'
   else
-    touch $2 ; echo 'IdPais;Nombre' > $2;
+    touch paises.txt ; echo 'IdPais;Nombre' > paises.txt;
   fi
- exit 0
 }
 
-verificar_path $1 $2 
+dirPersonas='personas.txt'
+dirPaises='paises.txt'
+
+verificar_path $dirPersonas $dirPaises 
+
+case $1 in
+	 
+	 -d|-D)
+	    
+		DNI=$2
+		awk  -F ";" '$2==var {print $0}' var="$DNI" $dirPersonas;;
+		
+	 -a|-A)
+         
+         awk -F ";" '$1=="" {printf $1";"$2";"$3";"$4}' $1>$1;;
+         
+     -e|-E)
+         
+         #awk -F ";" '$2 != 35537983 {print}' $1;;
+         awk '/35537983/ {while (/35537983/) ; next} 1' $1;;
+         
+     -p|-P)
+         
+         #awk -F ";" '$2 != 35537983 {print}' $1;;
+         awk - F ";" ' $4==1 {a[$1]=$4; next} $2 in a {print $1,$2,$3,a[$2]}' OFS=';' $2 $1;;
+         
+	*)    exit;;
+esac
