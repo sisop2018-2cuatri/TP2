810497136c0316c5fe7f36739e5852c30031fa50
Maximiliano Mercado <maximilianomercado.n@gmail.com>
2018-10-10 23:58:17 -0300

    Fix comentarios help y retoques finales a la funcionalidad

--- Ejercicio 2/TP2-Ejercicio2.sh
+++ Ejercicio 2/TP2-Ejercicio2.sh
@@ -3,12 +3,31 @@
 
 get_help() {
    
+    echo ""
     echo 'Nombre: ./TP2-Ejercicio2.sh <CantMax>'
+    echo ""
     echo 'Parametro: <CantMax> --- es un parametro con la cantidad maxima de veces a calcular'
+    echo ""
     echo 'Ejemplo: ./TP2-Ejercicio2.sh 5'
-    echo '-------------------------------------------------------------------'
-    echo 'Descripcion: Este script muestra de manera recursiva un parametro calculado una cantidad maxima de veces y guarda la secuencia de pasos en un archivo salida.txt'
-    exit
+    echo ""
+    echo 'Descripcion: Este script muestra de manera recursiva un parametro calculado una cantidad  maxima de veces y guarda la secuencia de pasos en un archivo salida.txt'
+    echo ""
+    echo ""
+    echo "Sistemas Operativos"
+    echo "-------------------"
+    echo "Trabajo Práctico N°2"
+    echo "Ejercicio 2"
+    echo "Script: ./TP2-Ejercicio2.sh"
+    echo "-------------------"
+    echo "Integrantes:"
+    echo "	Avila, Leandro - 35.537.983"
+    echo "	Di Lorenzo, Maximiliano - 38.166.442"
+    echo "	Lorenz, Lautaro - 37.661.245"
+    echo "	Mercado, Maximiliano - 37.250.369"
+    echo "	Sequeira, Eliana - 39.061.003"
+    echo ""
+    # fin mensaje de ayuda
+    exit 0
 }
 
 verificar_parametros(){
@@ -41,6 +60,7 @@ Calcular ()
 }
 
 verificar_parametros $1
+rm salida.txt
 Calcular 0 1 $1 1
 exit 0
 
